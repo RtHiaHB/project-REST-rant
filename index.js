eeb97ec0301b7e9b974e09492a57be9c39d4e028
Thomas Davidson <tomd1969@gmail.com>
2022-12-02 15:42:21 -0500

    Added CSS stylesheet to project.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Modules and globules
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
+//Express stuffs
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 const { PORT } = process.env
@@ -16,6 +21,7 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for connections
 app.listen(PORT, () => {
     console.log(`Currently listening on port ${PORT}...`);
 })
