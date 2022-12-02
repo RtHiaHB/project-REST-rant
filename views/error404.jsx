11924b20f5d6624625e3e4d979c801e9a445caf4
Thomas Davidson <tomd1969@gmail.com>
2022-12-02 16:01:21 -0500

    part 4.5 completed

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,7 @@ function Error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <img src='https://place-puppy.com/300x300' />
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function Home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img className='homeImg' src='/images/pablo-pacheco-pizza.jpg' alt='Yummy pizza!' />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@pablopacheco_videografo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pablo Pacheco</a> on <a href="https://unsplash.com/images/food/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href='/places'>
                     <button className="btn-primary">Places Page</button>
                 </a>
