dace6831e776dad0de6f106df945e8f8d7ec45b1
Thomas Davidson <tomd1969@gmail.com>
2022-12-03 04:17:44 -0500

    Adding and rendering a new view (Try #2)

---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/jerome-jome-h-thai-ml-unsplash.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/nathan-dumlao-coding-cat-cafe-unsplash.jpg'
+}]
+
