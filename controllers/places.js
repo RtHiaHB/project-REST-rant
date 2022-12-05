96f1173c91ade91af39ecb270a495e9457e8f13d
Thomas Davidson <tomd1969@gmail.com>
2022-12-04 22:01:07 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -16,8 +16,12 @@ router.get('/:id', (req, res) => {
     } else if (!places[id]) {
         res.render('error404')
     } else {
-        res.render('places/show', { place: places[id] })        
+        res.render('places/show', { place: places[id], id })        
     }
+})
+
+router.get('/:id/edit', (req, res) => {
+    res.send(`Edit STUB ${places[req.params.id].name}`)
 })
 
 router.post('/', (req, res) => {
@@ -33,6 +37,17 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+})
+router.delete('/:id', (req, res) => {
+    let id=Number(req.params.id)
+    if(isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
 })
 
 module.exports = router
