f0181422f5e3d17505d1ced4c3b9bb05a3ce0362
Thomas Davidson <tomd1969@gmail.com>
2022-12-22 23:10:28 -0500

    removed reference to controllers

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // Controllers and Routes
-app.use('/places', require('./controllers/places'))
+//app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     //res.render('home')
